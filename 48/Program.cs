69578e4ae28f2aa60b7266d5c01700347935e84e
DanteMilten <milten47@yahoo.com>
2023-02-17 23:11:19 +0300

    48

--- 48/Program.cs
+++ 48/Program.cs
@@ -8,14 +8,28 @@
 // 1 2 3 4
 // 2 3 4 5
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+int[,] GetArray(int m, int n)
 {
     int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = i+j;
         }
     }
     return result;
