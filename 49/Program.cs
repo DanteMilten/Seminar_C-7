ae597a5278154be28a1e07120c55b9aed3cc766d
DanteMilten <milten47@yahoo.com>
2023-02-17 23:21:45 +0300

    49

--- 49/Program.cs
+++ 49/Program.cs
@@ -22,7 +22,66 @@
 
 // 1 **4** 3 **16**
 
-Console.Clear();
+using System;
+using static System.Console;
+
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns,0,10);
+PrintArray(array);
+WriteLine();
+//ChangeArray(array);
+PrintArray(ChangeArray(array));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max+1);
+        }
+
+    }
+    return result;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+/*Console.Clear();
 Console.WriteLine();
 
 int[,] GetArray(int row, int col, int min, int max)
@@ -73,4 +132,4 @@ int[,] mainArray = GetArray(row, col, min, max);
 PrintArray(mainArray);
 Console.WriteLine("--------");
 int[,] chagedArray = ChangeToSquareEvenIndex(mainArray);
-PrintArray(chagedArray);
+PrintArray(chagedArray);*/
